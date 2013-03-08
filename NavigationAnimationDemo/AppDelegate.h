6de1cc79d1703ad0605308bf171462cde5167049
yangning <buaa.yangning@gmail.com>
2013-03-08 15:27:04 +0800

    Initial Commit

---
+++ NavigationAnimationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NavigationAnimationDemo
+//
+//  Created by 杨宁 on 13-3-8.
+//  Copyright (c) 2013年 numbbuaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
