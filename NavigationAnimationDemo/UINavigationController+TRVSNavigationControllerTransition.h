f0555bf72568d7cd57e51f2d13acb58f58774ec9
yangning <buaa.yangning@gmail.com>
2013-08-23 11:09:27 +0800

    添加注释

--- NavigationAnimationDemo/UINavigationController+TRVSNavigationControllerTransition.h
+++ NavigationAnimationDemo/UINavigationController+TRVSNavigationControllerTransition.h
@@ -9,6 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface UINavigationController (TRVSNavigationControllerTransition)
+
 - (void)pushViewControllerWithNavigationControllerTransition:(UIViewController *)viewController;
+
 - (void)popViewControllerWithNavigationControllerTransition;
+
+/**
+ *	@brief	push到下一级页面（带navbar和tabbar的动画）
+ *
+ *	@param 	viewController 	下一级页面的视图控制器
+ */
+- (void)pushViewControllerWithNavigationControllerTransitionAndBarAnimation:(UIViewController *)viewController;
+
+/**
+ *	@brief	pop到上一级页面（带导航条动画）
+ */
+- (void)popViewControllerWithNavigationControllerTransitionAndBarAnimation;
+
 @end
